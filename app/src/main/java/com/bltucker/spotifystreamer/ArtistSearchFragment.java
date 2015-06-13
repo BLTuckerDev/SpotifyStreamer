ed6b44f60106647dd6fc13c7b5665192336dc6ba
Brett Tucker <bretttucker@gmail.com>
2015-06-13 10:16:14 -0600

    Libraries and Initial UI
    This commit adds dependencies on the spotify api wrapper as well as
    Picasso.
    
    It also includes the initial xml layout files that will display the
    user's artist searches.

--- app/src/main/java/com/bltucker/spotifystreamer/ArtistSearchFragment.java
+++ app/src/main/java/com/bltucker/spotifystreamer/ArtistSearchFragment.java
@@ -7,18 +7,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class MainActivityFragment extends Fragment {
+public class ArtistSearchFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public ArtistSearchFragment() {
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        return inflater.inflate(R.layout.fragment_artist_search, container, false);
     }
 }
